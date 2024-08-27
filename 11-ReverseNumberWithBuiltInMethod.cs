a333308cc8962b67d021f5214147465caed92d64
pooja bhaumik <pbhaumik200@gmail.com>
2024-08-27 18:06:11 +0530

    reverse a number with built in method

---
+++ 11-ReverseNumberWithBuiltInMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    public static class ReverseNumberWithBuiltInMethod
+    {
+        public static long ReverseNumberWithBuiltMethod()
+        {
+            int number = 123;
+            char[] numArray = number.ToString().ToCharArray();
+            Array.Reverse(numArray);
+            long result = Convert.ToInt64(numArray);
+            return result;
+
+        }
+        
+        
+    }
+}
